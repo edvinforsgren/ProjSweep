874b345ca4ef51dfb89a856e7b86557536bd63f0
Edvin Forsgren <edvin.forsgren@umu.se>
2022-01-19 11:52:49 +0100

    Updated user friendly

---
+++ src/run.py
@@ -0,0 +1,21 @@
+from CGAN_train import run_training
+
+
+def read_file_lines(path_to_config):
+    with open(path_to_config) as f:
+        lines = f.readlines()
+
+    rm_lines = []
+    for line in lines:
+        if line.strip().startswith("#"):
+            rm_lines.append(line)
+    for rm_line in rm_lines:
+        lines.remove(rm_line)
+    return lines
+
+
+# Specify your absolute path to your config.txt
+path_to_config = "/media/data1/projsweep3D/src/config.txt"
+lines = read_file_lines(path_to_config)
+save_dir, input_path, target_path, n_epochs = [line[:-1] for line in lines]
+run_training(save_dir=save_dir, target_path=target_path, input_path=input_path, n_epochs=n_epochs)
