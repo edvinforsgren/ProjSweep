067c28564c7391fb91be48ea1edaf2f5abc2cf8d
Edvin Forsgren <edvin.forsgren@umu.se>
2022-01-19 11:52:49 +0100

    Updated user friendly

--- src/context.py
+++ src/context.py
@@ -1,5 +1,5 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname("/media/data1/projsweep3D/tools/"))))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname("../tools/"))))
 import dl_imaging_kit as dlik
 from dl_imaging_kit.lightning.gan import CGANModule
